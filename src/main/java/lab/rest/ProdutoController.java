1d4f7dc33c8c4d1bcf1c8a0c2589af8e0f9c2fc0
Walison Moreira <walison.moreira@gmail.com>
2016-09-10 17:33:06 -0300

    Não tá pronto ainda!

--- src/main/java/lab/rest/ProdutoController.java
+++ src/main/java/lab/rest/ProdutoController.java
@@ -5,6 +5,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,8 +33,8 @@ public class ProdutoController {
   }
 
   @RequestMapping(value = "/novo", method = POST)
-  public List<Produto> novo(Produto produto) {
+  public Produto novo(@RequestBody Produto produto) {
     produtos.add(produto);
-    return produtos;
+    return produto;
   }
 }
