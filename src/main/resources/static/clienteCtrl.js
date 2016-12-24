3d0eaff47683e5980a70a73f167e1e9bd85b616a
Walison Moreira <walison.moreira@gmail.com>
2016-12-23 23:41:12 -0200

    Utilização do "localtunel".

--- src/main/resources/static/clienteCtrl.js
+++ src/main/resources/static/clienteCtrl.js
@@ -1,6 +1,6 @@
 angular.module("vendas").controller("clienteCtrl", function ($scope, $http) {
 		$scope.novoCliente = function (cliente) {
-			$http.post("cliente/novo", cliente).success(function (resultadoDoResponse) {
+			$http.post("http://venda.localtunnel.me/cliente/novo", cliente).success(function (resultadoDoResponse) {
 				$scope.cliente = resultadoDoResponse;
 			});
 		};		
--- src/main/resources/static/produtoCtrl.js
+++ src/main/resources/static/produtoCtrl.js
@@ -1,6 +1,6 @@
 angular.module("vendas").controller("produtoCtrl", function ($scope, $http) {
 		$scope.novoProduto = function (produto) {
-			$http.post("produto/novo", produto).success(function (resultadoDoResponse) {
+			$http.post("http://venda.localtunnel.me/produto/novo", produto).success(function (resultadoDoResponse) {
 				$scope.produto = resultadoDoResponse;
 			});
 		};		
